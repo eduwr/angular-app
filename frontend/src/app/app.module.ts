a32ff11cc4fcd6eda41574a7005b0d0198d3b874
eduwr <eduwric@gmail.com>
2021-08-31 00:47:00 -0300

    feat: create product service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 import { ProdCrudComponent } from "./views/prod-crud/prod-crud.component";
 
@@ -41,6 +42,7 @@ import { ProductCreateComponent } from "./components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ProductService } from "../product.service";
 
 @Component({
   selector: "app-product-create",
@@ -6,7 +8,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
-  constructor() {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  saveProduct() {
+    this.productService.showMessage("test messaeg");
+  }
+
+  cancelProduct() {
+    this.router.navigate(["/products"]);
+  }
 }
