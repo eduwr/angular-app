afceeeab522f53dd078e77d8e5ad4ddcb79eb9d6
eduwr <eduwric@gmail.com>
2021-08-31 22:14:00 -0300

    feat: update component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -25,6 +25,17 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { ProductReadComponent } from "./components/product/product-read/product-read.component";
+import { ProductRead2Component } from "./components/product/product-read2/product-read2.component";
+import { MatTableModule } from "@angular/material/table";
+
+import { MatSortModule } from "@angular/material/sort";
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,6 +46,9 @@ import { MatInputModule } from "@angular/material/input";
     ProdCrudComponent,
     RedDirective,
     ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,8 +65,16 @@ import { MatInputModule } from "@angular/material/input";
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "pt-BR",
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
+
 import { Product } from "./product.model";
 
 @Injectable({
@@ -20,7 +22,33 @@ export class ProductService {
     });
   }
 
-  create(product: Product): Observable<Product> {
+  create(product: Omit<Product, "id">): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product);
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  getProductById(id: number): Observable<Product | undefined> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`).pipe(
+      catchError((error) => {
+        this.showMessage(error.message);
+        return of(undefined);
+      })
+    );
+  }
+
+  update(product: Product): Observable<Product | undefined> {
+    let _product: Product | undefined;
+    this.getProductById(product.id).subscribe(
+      (response) => (_product = response)
+    );
+
+    if (!_product) {
+      return of(undefined);
+    }
+
+    return this.http.patch<Product>(`${this.baseUrl}/${_product.id}`, _product);
   }
 }
